a3183e2590161c1ac801d37c119d4d36f4e3780e
usc-T_P111 <t_p111@student.usc.edu.au>
2025-08-15 15:57:04 +1000

    Moved JS init from head to body

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 anime({
-  targets: 'div',
-  translateX: 250,
-  duration: 2000
+  targets: ".ball",
+  scale: [
+    { value: 1.5, duration: 1000, easing: "easeInOutQuad" },
+    { value: 1, duration: 1000, easing: "easeInOutQuad" },
+  ],
+  loop: true,
 });
